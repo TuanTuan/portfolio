e0f2ea6c3cbfe55bd6f4ca97d62badc411f57827
TuanTuan <mengzhao_email@qq.com>
2025-08-28 17:13:39 +0800

    add tailwind css

---
+++ portfolio/app/pages/about.vue
@@ -0,0 +1,19 @@
+<template>  
+  <div>
+    <h1>This is about page</h1>
+  </div>
+  <div>
+    <button @click="enableCustomLayout">更新布局</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+definePageMeta({
+  layout: 'another'
+})
+
+function enableCustomLayout () {
+  setPageLayout('default')
+}
+</script>
